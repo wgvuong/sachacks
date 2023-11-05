2c278a91becaaf0764a66badcc7cd125047be923
Kelly Phan <kelly@Kellys-MacBook-Pro-9.local>
2023-11-05 01:44:46 -0700

    rendering smiskis

---
+++ main.py
@@ -0,0 +1,148 @@
+from datetime import datetime, timedelta
+from flask import Flask, redirect, request, jsonify, session, render_template
+import requests
+import urllib.parse
+import json
+import os
+
+SMISKI_FOLDER = os.path.join('static', 'smiski_photo')
+
+CLIENT_ID = "5b333ffe3af944b390112ecfdf577600"
+CLIENT_SECRET = "9e2a183f3d8c448aa53779d8bc52ac1b"
+
+app = Flask(__name__)
+app.secret_key = '36zRc2jMKTWGM1Fg6xi1g44912932496fququBL5vX'
+app.config['UPLOAD_FOLDER'] = SMISKI_FOLDER
+
+REDIRECT_URI ='http://localhost:5000/callback'
+AUTH_URL = 'https://accounts.spotify.com/authorize'
+TOKEN_URL = 'https://accounts.spotify.com/api/token'
+API_BASE_URL = 'https://api.spotify.com/v1/'
+
+#root
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/login')
+def login():
+    scope ='user-read-private user-read-email user-top-read'
+
+    params = {
+        'client_id' :CLIENT_ID,
+        'response_type': 'code',
+        'scope': scope,
+        'redirect_uri': REDIRECT_URI,
+        'show_dialog': True
+    }
+
+    auth_url = f"{AUTH_URL}?{urllib.parse.urlencode(params)}"
+    return redirect(auth_url)
+
+@app.route('/callback')
+def callback():
+    if 'error' in request.args:
+        return jsonify({"error": request.args['error']})
+    
+    if 'code' in request.args:
+        req_body = {
+            'code' : request.args['code'],
+            'grant_type' : 'authorization_code',
+            'redirect_uri' : REDIRECT_URI,
+            'client_id': CLIENT_ID,
+            'client_secret' : CLIENT_SECRET
+        }
+        response = requests.post(TOKEN_URL, data=req_body)      
+        token_info = response.json()
+
+        session['access_token'] = token_info['access_token']
+        session['refresh_token'] = token_info['refresh_token']
+        session['expires_at'] =  datetime.now().timestamp() + token_info['expires_in']
+        return redirect('/playlists')    
+
+@app.route('/playlists')
+def get_playlist():
+    if 'access_token' not in session:
+        return redirect('/login')
+    
+    if datetime.now().timestamp() > session['expires_at']:
+        return redirect('/refresh-token')
+    
+    headers = {
+        'Authorization' : f"Bearer {session['access_token']}"
+    }
+
+    response = requests.get("https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=10", headers=headers)
+    
+    songs = json.loads(response.content)["items"]
+    ids = []
+    for idx, song in enumerate(songs):
+        ids.append(song["id"])
+    
+    id_string = f"{ids[0]},{ids[1]},{ids[2]},{ids[3]},{ids[4]},{ids[5]},{ids[6]},{ids[7]},{ids[8]},{ids[9]}"
+    song_response = requests.get("https://api.spotify.com/v1/audio-features?ids=" + id_string, headers=headers)
+    song_data = song_response.json()
+
+    traits = {'acousticness': 0.0, 
+              'danceability': 0.0, 
+              'energy': 0.0, 
+              'instrumentalness': 0.0, 
+              'liveness': 0.0, 
+              'loudness': 0.0,
+              'speechiness': 0.0, 
+              'valence': 0.0
+            }
+    
+    for idx, feature in enumerate(song_data['audio_features']):
+        for key in traits:
+            traits[key] += abs(feature[key])
+    for key in traits:
+        traits[key] = traits[key] / 10
+    traits['loudness'] = (1 - (traits['loudness']) / 60) * 0.75
+    personality = max(traits, key=traits.get)
+
+    match personality:
+        case 'acousticness':
+            full_filename = os.path.join(app.config['SMISKI_FOLDER'], 'smiski_placeholder.webp')
+            return render_template("results.html", image_path=full_filename)
+        case 'danceability':
+            return render_template("results.html", image_path="")
+        case 'energy':
+            full_filename = os.path.join(app.config['SMISKI_FOLDER'], 'smiski_placeholder.webp')
+            return render_template("results.html", image_path=full_filename)
+        case 'instrumentalness':
+            return render_template("results.html", image_path="")
+        case 'liveness':
+            return render_template("results.html", image_path="")
+        case 'loudness':
+            return render_template("results.html", image_path="")
+        case 'speechiness':
+            return render_template("results.html", image_path="")
+        case 'valence':
+            return render_template("results.html", image_path="")
+
+
+@app.route('/refresh-token')
+def refresh_token():
+    if 'refresh_token' not in session:
+        return redirect('/login')
+    
+    if datetime.now().timestamp() > session['expires_at']:
+        req_body = {
+            'grant_type' : 'refresh_token',
+            'refresh_token' : session['refresh_token'],
+            'client_id': CLIENT_ID,
+            'client_secret' : CLIENT_SECRET
+        }
+
+        response = requests.post(TOKEN_URL, data=req_body)
+        new_token_info = response.json
+        session['access_token'] = new_token_info['access_token']
+        session['expires_at'] =  datetime.now().timestamp() + new_token_info['expires_in']
+        return redirect('/playlists')
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0', debug=True)
+
+
